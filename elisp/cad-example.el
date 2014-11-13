2b0a74b774fc015794b554e175db306605884fc5
John Sturdy <jcg.sturdy@gmail.com>
2014-11-13 23:33:03 +0000

    Add example

---
+++ elisp/cad-example.el
@@ -0,0 +1,35 @@
+;;; cad-example.el --- examples and tests for cad.el
+
+;; Copyright (C) 2014  John Sturdy
+
+;; Author: John Sturdy <john.sturdy@arm.com>
+;; Keywords: multimedia
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(require 'cad)
+
+(drawing just-a-circle 400 400
+	 (shape cutpath
+		(moveto 200 200)
+		(circle 100)))
+
+(provide 'cad-example)
+;;; cad-example.el ends here
