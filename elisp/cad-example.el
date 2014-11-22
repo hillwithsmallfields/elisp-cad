0de0e1a93507d08eb30367da1ecda714eadae852
John Sturdy <jcg.sturdy@gmail.com>
2014-11-22 20:38:43 +0000

    Add some new examples, and reformat.

--- elisp/cad-example.el
+++ elisp/cad-example.el
@@ -27,35 +27,36 @@
 (require 'cad)
 
 (drawing just-a-circle cutpath 400 400
-	 (shape
-	  (moveto 200 200)
-	  (cad-circle 100)))
+	 (shape (moveto 200 200)
+		(cad-circle 100)))
 
 (drawing just-a-rectangle cutpath 400 400
-	 (shape
-	  (moveto 200 200)
-	  (cad-rectangle 150 100)))
+	 (shape (moveto 200 200)
+		(cad-rectangle 150 100)))
 
 (drawing fancy-rectangle cutpath 400 400
 	 (rectangle width 200 height 200 left 100 top 300))
 
+(drawing stacked-rectangles cutpath 800 1200
+	 (shape (rectangle left 100 bottom 100 height 200 width 600)
+		(rectangle left 150 bottom 300 height 100 width 500)
+		(rectangle left 200 bottom 400 height 50 width 400)))
+
 (drawing tilted-rectangle cutpath 400 400
-	 (shape
-	  (rotate 15
-		  (moveto 200 200)
-		  (cad-rectangle 150 100 "tilted"))))
+	 (shape (rotate 15
+			(moveto 200 200)
+			(cad-rectangle 150 100 "tilted"))))
 
 (drawing arc-fragment cutpath 400 400
-	 (shape
-	  (moveto 200 200)
-	  (cad-circle 50)
-	  (cad-arc 200 200 100 0 75)))
+	 (shape (moveto 200 200)
+		(cad-circle 50)
+		(cad-arc 200 200 100 0 75)))
 
 (drawing many-arcs cutpath 1400 1400
+	 (shape (rectangle bottom 10 left 10 width 200 height 20))
 	 (dotimes (i 12)
 	   (dotimes (j 12)
-	     (shape
-	      (cad-arc (* j 50) (* i 50) 12 (* j 30) (* i 30))))))
+	     (shape (cad-arc (* j 50) (* i 50) 12 (* j 30) (* i 30))))))
 
 (provide 'cad-example)
 ;;; cad-example.el ends here
